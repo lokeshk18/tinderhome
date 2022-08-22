a45d8d5995962460e92c8f9bcfd514ae89b7b0ef
lokeshk18 <lokeshkl12318@gmail.com>
2022-08-22 22:27:33 +0530

    Initial Commmit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './component/Header';
+import TinderCards from './component/TinderCards';
+import Footer from './component/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <TinderCards />
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
